8f45746dd4e584b0e94aa80dbe55b47fdcd591a9
Lex von Biedenfeld <avonbied@users.noreply.github.com>
2020-09-11 23:46:56 -0500

    Update footer style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import '../styles/footer.scss';
 
-export default function Footer() {
-    return(
+const Footer = () => {
+    return (
         <footer>
             <div>
                 <a >LinkedIn</a>
                 <a >Facebook</a>
                 <a >Instagram</a>
             </div>
+            <div>
+                <cite></cite>
+            </div>
         </footer>
     );
 }
+
+export default Footer;
