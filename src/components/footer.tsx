03d8db3d7cd54a28d573aeb833d2277210a3cd9f
Lex von Biedenfeld <avonbied@users.noreply.github.com>
2020-09-13 14:14:30 -0500

    Update footer to include FontAwesome

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,21 +3,21 @@ import '../styles/footer.scss';
 
 const socialLinks = [
     {
-        name: 'LinkedIn',
+        name: 'linkedin',
         link: 'https://www.linkedin.com/company/compscia'
     },
     {
-        name: 'Facebook',
+        name: 'facebook-square',
         link: 'https://www.facebook.com/compscia'
     },
     //<i class="fa fa-linkedin-square"></i><i class="fa fa-instagram"></i><i class="fa fa-facebook"></i>
     {
-        name: 'Instagram',
+        name: 'instagram-square',
         link: 'https://www.instagram.com/comp.scia'
 
     },
     {
-        name: 'github',
+        name: 'github-square',
         link: 'https://github.com/comp-scia'
     }
 ]
@@ -27,11 +27,8 @@ const Footer = () => {
         <footer>
             <div className="social-icons">
                 { socialLinks.map((link,i) => (
-                    <a href={link.link} key={i}>{link.name}</a>
+                    <a href={link.link} className="icon" key={i}><i className={"fab fa-".concat(link.name)}></i></a>
                 )) }
-                <a >LinkedIn</a>
-                <a >Facebook</a>
-                <a >Instagram</a>
             </div>
             <div className="copyright">
                 <small>&copy; 2019-{ (new Date()).getFullYear() } Computer Science Association</small>
