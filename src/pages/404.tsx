1568c36492b228adb4f95765ace6e5397f4f7b72
Lex von Biedenfeld <avonbied@users.noreply.github.com>
2020-11-04 17:44:37 -0600

    Add 404 page and placeholders

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <>
+        <h2 style={{textAlign: 'center'}}>Oops. We couldn't find that page</h2>
+        <h3 style={{textAlign: 'center'}}>Please check to make sure that you type in the correct url.</h3>
+        </>
+    );
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default () => {
     return (
         <>
-            
+        <section>
+            <h2 style={{textAlign:'center', paddingBottom: '24px'}}>COMING SOON</h2>
+            <p style={{textAlign: 'center'}}>More content coming soon. In the meantime checkout our GitHub here: <a href='https://github.com/comp-scia/getting-started' style={{color: 'blue'}}>comp-scia</a></p>
+        </section>
         </>
     );
 }
